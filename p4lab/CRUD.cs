790af78533403a97eb1e816f78625abfa0cd5aed
seba300 <sebastiandomagala98@gmail.com>
2020-02-16 13:05:13 +0100

    first commit

---
+++ p4lab/CRUD.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+namespace p4lab
+{
+    class CRUD
+    {
+        public void Create(int id,string regionDescription, SqlConnection conn)
+        {
+            string sql = "INSERT INTO Northwind.dbo.Region(RegionID,RegionDescription) VALUES (@id,@regionName)";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@regionName", regionDescription);
+            command.Parameters.AddWithValue("@id", id);
+
+            //returns numbers of lines
+            int x = command.ExecuteNonQuery();
+
+            if(x>0)
+            {
+                Console.WriteLine("Wpisane");
+            }
+        }
+        public void Update(int id, string newName, SqlConnection conn)
+        {
+            string sql = "UPDATE Northwind.dbo.Region SET RegionDescription = @regionName WHERE RegionID=@id";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@regionName", newName);
+            command.Parameters.AddWithValue("@id", id);
+
+            int x = command.ExecuteNonQuery();
+
+            if (x > 0)
+            {
+                Console.WriteLine("Zmieniono");
+            }
+        }
+
+        public void Delete(int id, SqlConnection conn)
+        {
+            string sql = "DELETE FROM Northwind.dbo.Region WHERE RegionID=@id";
+            var command = new SqlCommand(sql, conn);
+            command.Parameters.AddWithValue("@id", id);
+
+            int x = command.ExecuteNonQuery();
+
+            if (x > 0)
+            {
+                Console.WriteLine("Usunieto");
+            }
+        }
+
+    }
+}
---
+++ p4lab/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace p4lab
+{
+    //ADO.NET
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString =
+                @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using SqlConnection conn = new SqlConnection(connectionString);
+
+            conn.Open();
+
+            string CommandText = "SELECT * FROM Northwind.dbo.Customers";
+            SqlCommand command = new SqlCommand(CommandText, conn);
+
+            //execute commandText
+            SqlDataReader reader = command.ExecuteReader();
+
+            //read all
+            while (reader.Read())
+            {
+                //Console.WriteLine(reader["CompanyName"]);
+            }
+            reader.Close();
+
+            CRUD crud = new CRUD();
+            //crud.Create(7,"Slask", conn);
+
+            //crud.Update(6, "NieSlask", conn);
+
+            crud.Delete(6, conn);
+            conn.Close();
+        }
+    }
+}
